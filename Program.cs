cbfff7bba44fee1bb11097e6cae9256aa875ec8c
Lacey Walker <bluelarksong@gmail.com>
2020-11-20 10:15:35 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Guess a number: ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                int inputNum = int.Parse(input);
+                Console.WriteLine($"your guess was {inputNum}");
+
+                Console.WriteLine(1000 / inputNum);
+            }
+
+            catch
+            {
+                Console.WriteLine($"not a valid number");
+            }
+
+        }
+    }
+}
